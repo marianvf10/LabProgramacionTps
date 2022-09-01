05cd8c31bc366bf27a0f3d9df7747d6a6ee635ce
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2022-08-31 21:50:53 -0300

    Se agrego callable para funcionalidad 6

---
+++ Compartido/AlumnoCasiExpulsado.java
@@ -0,0 +1,37 @@
+package Compartido;
+
+public class AlumnoCasiExpulsado implements Callable {
+
+    private BaseBatos baseDatos;
+
+    public AlumnoCasiExpulsado(BaseBatos b) {
+        this.baseDatos = b;
+    }
+
+    public Object call() {
+        int amonestaciones[][] = this.baseDatos.getAmonestaciones();
+        int cantAlumApuntoSerExpulsado, cantAlumnos, cantMeses, cantAmonestaciones;
+        cantAlumApuntoSerExpulsado = 0;
+        cantAlumnos = baseDatos.getCantAlumnos();
+        cantMeses = baseDatos.getCantMaterias();
+        cantAmonestaciones = 0;
+
+        for (int i = 0; i < cantAlumnos; i++) {
+            for (int j = 0; j < cantMeses; j++) {
+                if (amonestaciones[i][j] > 0) {
+                    cantAmonestaciones++;
+                }
+                if (cantAmonestaciones == 3) {
+                    // este if es para cortar cuando ya se detecta que el alumno tiene 3
+                    // amonestaciones
+                    j = cantMeses;
+                }
+            }
+            if (cantAmonestaciones > 2) {
+                cantAlumApuntoSerExpulsado++;
+            }
+            cantAmonestaciones = 0;
+        }
+        return cantAlumApuntoSerExpulsado;
+    }
+}
