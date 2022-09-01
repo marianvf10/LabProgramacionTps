b2fef7f70462a209e37983e18440aef1e507a054
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2022-08-31 21:28:23 -0300

    Clase callable para funcionalidad con opcion 4

---
+++ Compartido/AlumnoMenosCincoAprobada.java
@@ -0,0 +1,41 @@
+package Compartido;
+
+import java.util.concurrent.Callable;
+
+public class AlumnoMenosCincoAprobada implements Callable {
+    
+    private BaseBatos baseDatos;
+
+    public AlumnoMenosCincoAprobada(BaseBatos b){
+        this.baseDatos = b;
+    }
+
+    public Object call() {
+        int nota[][] = this.baseDatos.getNotas();
+        int cantAlumMenosCincoMateriasAprobadas, cantAlumnos, cantMaterias,cantMateriasAprobadas;
+        cantAlumMenosCincoMateriasAprobadas = 0;
+        cantAlumnos = baseDatos.getCantAlumnos();
+        cantMaterias = baseDatos.getCantMaterias();
+        cantMateriasAprobadas = 0;
+        
+        for (int i = 0; i < cantAlumnos; i++){
+            for(int j = 0; j < cantMaterias; j++){
+                if (nota[i][j] >= 6){
+                    cantMateriasAprobadas++;
+                }
+                if (cantMateriasAprobadas == 3){
+                    j = cantMaterias;
+                }
+                }
+                if (cantMateriasAprobadas < 3){
+                    cantAlumMenosCincoMateriasAprobadas++;
+                }
+                cantMateriasAprobadas = 0;
+            }
+
+        
+        return cantAlumMenosCincoMateriasAprobadas;
+
+    }
+    
+}
