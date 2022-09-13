9dab9b4850504edf5fbd77bdd157c602afa767c6
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2022-09-13 17:31:58 -0300

    main de prueba

--- PatronDecorator/main.java
+++ PatronDecorator/main.java
@@ -1,5 +1,11 @@
-public class main {
+public class Main {
     public static void main(String args[]){
-        
+        PersonajeBasico p1 = new PersonajeBasico("MARIANO");
+        PersonajeBasico p2 = new PersonajeBasico("ADRIANO");
+        p1.atacar(p2);
+        System.out.println();
+        GemaGuerrero gemaGuerrero = new GemaGuerrero(p1);
+        gemaGuerrero.atacar(p2);
+
     }
 }
