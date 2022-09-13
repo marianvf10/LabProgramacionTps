e974d02d3e5cfaa96bac271c1ce73495963c4130
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2022-09-13 17:31:26 -0300

    Decorador de Gema de Curador añadida

---
+++ PatronDecorator/GemaCurador.java
@@ -0,0 +1,35 @@
+public class GemaCurador extends PersonajeDecorator {
+
+    public GemaCurador(Personaje personaje) {
+        super(personaje);
+        //TODO Auto-generated constructor stub
+    }
+
+    public void curarAliado(PersonajeBasico personaje){
+        //el aliado tiene una curacion de 15 unidades
+        int vidaRestante = personaje.getVida();
+        if (vidaRestante < 86) {
+            int vidaActualizada = vidaRestante+15;
+            personaje.setVida(vidaActualizada);
+            System.out.println(this.getNombre()+" curo a "+personaje.getNombre()+" ahora su vida es: "+personaje.getVida());
+        }
+
+    }
+
+    public void otorgarEscudoAliado(GemaGuerrero personaje){
+        //El curandero solo otorga escudos a guerreros
+        int escudo = 20;
+        if (personaje.getEscudo() == 0){
+            personaje.setEscudo(escudo);
+        }
+       
+
+    }
+
+    public void revivirAliado(PersonajeBasico personaje){
+        if (personaje.getVida() == 0){
+            personaje.setVida(100);
+        }
+    }
+    
+}
