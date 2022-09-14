02ff65097ac3bae9ba4695b6a6bde70d13226b96
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2022-09-14 00:57:50 -0300

    se agregaron metodos de estado del personaje y se cambiaron tipo de parametros en metodos

--- PatronDecorator/Personaje.java
+++ PatronDecorator/Personaje.java
@@ -1,20 +1,20 @@
 public interface Personaje{
  
-    public abstract void recibirDaño(double dañoAtaque);
+    public abstract void recibirDaño(int dañoAtaque);
 
-    public abstract void defenderse(double dañoAtaque);
-    
+    public abstract void defenderse(int dañoAtaque);
     
     public abstract void atacar(PersonajeBasico perso);
 
     public abstract void escapar();
 
-
     public abstract int getDaño();
 
     public abstract int getVida();
 
-    public abstract int getVelocidadAtaque();
+    public abstract boolean getEstado();
+
+    public abstract void setEstado();
 
     public abstract String getNombre();
 
