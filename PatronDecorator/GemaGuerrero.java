5f10ba8cd6e13983449cf7c1087e39e7f12c4adf
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2022-09-14 01:01:41 -0300

    Se cambiaron tipos de parametros en los metodos

--- PatronDecorator/GemaGuerrero.java
+++ PatronDecorator/GemaGuerrero.java
@@ -1,25 +1,25 @@
 public class GemaGuerrero extends PersonajeDecorator {
-    private int escudo;
+   
     public GemaGuerrero(Personaje personaje){
         super(personaje);
-        this.escudo = 10;
+       
     }
 
     @Override
-    public void recibirDaño(double dañoAtaque) {
+    public void recibirDaño(int dañoAtaque) {
         // TODO Auto-generated method stub
         
     }
 
     @Override
-    public void defenderse(double dañoAtaque) {
+    public void defenderse(int dañoAtaque) {
         // TODO Auto-generated method stub
         
     }
 
     @Override
     public void atacar(PersonajeBasico perso) {
-        double dañoAInfrigir = 0;
+        int dañoAInfrigir = 0;
         /*se calcula el daño con el critico de 50% */
         if (dañoAInfrigir >= perso.getVida()){
             this.matar(perso,dañoAInfrigir);
@@ -28,27 +28,11 @@ public class GemaGuerrero extends PersonajeDecorator {
         }
     }
 
-    private void matar(PersonajeBasico perso,double dañoAtaque){
+    private void matar(PersonajeBasico perso,int dañoAtaque){
         perso.recibirDaño(dañoAtaque);
         perso.setVida(0);
         System.out.println(this.getNombre()+" ejecuta a "+perso.getNombre()+"ya no puede participar en la batalla");
     }
-
-    public int getEscudo(){
-        return this.escudo;
-    }
-
-    public void setEscudo(int escudo){
-        this.setEscudo(escudo);
-        try {
-            Thread.sleep(5000);
-            //luego de 5 segundos desaparece el escudo
-            this.setEscudo(0);
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-    }
-
 
     
 }
