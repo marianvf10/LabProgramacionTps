c7f19d294df307e58bdadea29f1e1c088caa2db6
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2022-09-14 01:00:42 -0300

    Se modifico metodo de defensa de tanque

--- PatronDecorator/GemaTanque.java
+++ PatronDecorator/GemaTanque.java
@@ -1,44 +1,38 @@
+import java.util.Random;
+
 public class GemaTanque extends PersonajeDecorator {
 
-    private int armadura;
     public GemaTanque(Personaje personaje) {
         super(personaje);
-        this.armadura = 50;
-    
-    }
-    @Override
-    public void recibirDaño(double dañoAtaque) {
-        // TODO Auto-generated method stub
         
     }
-    public double defenderseTanque(double dañoAtaque){
-        double dañoRetorno = 0;
-        double dañoRecibido = this.armadura - dañoAtaque;
-        if (dañoRecibido < 0) {
-            double daño = dañoRecibido*-1;
-            //se rompio el escudo, se debe restar vida
-            this.recibirDaño(daño);
-        } else {
-            this.armadura -= dañoAtaque;
+  
+    public int defenderseTanque(int dañoAtaque){
+        defenderse(dañoAtaque);
+        int dañoRetornado = 0;
+        if (esCritico()){
+            //se calcula la probabilidad de critico
+            dañoRetornado = dañoAtaque;
         }
-        /*se calcula la probabilidad de retornar el daño */
-        return dañoRetorno;
+        return dañoRetornado;
     }
-    @Override
-    public void atacar(PersonajeBasico perso) {
-        // TODO Auto-generated method stub
-        
-    }
+
     @Override
     public void escapar() {
         System.out.println(this.getNombre()+" escapando...");
-        try {
-            Thread.sleep(4000);
-        } catch (Exception e) {
-        }
+  
         System.out.println(this.getNombre()+" se escapo");
         
     }
-    
+
+    private boolean esCritico(){
+        Random r = new Random();
+        int valor = r.nextInt(100);
+        return valor % 2 == 0;
+    }
+
+    public void protegerAliado(){
+        
+    }
     
 }
